dbee33b07f4134170f81f75a925943e31ac16a06
Huy <quochuybt2005@gmail.com>
2026-05-28 00:39:09 +0700

    create music app

---
+++ backend/src/main/java/com/quochuy/musicapp/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.quochuy.musicapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Spring Boot chạy thành công!";
+    }
+}
